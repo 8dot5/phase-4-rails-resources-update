9a38d3ba00f8addf05be591cd5866c5db5112f35
Chun Yi <8.5by11@gmail.com>
2022-03-15 21:45:30 -0400

    codealong

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create, :update]
+
+  #custom routes: all the frontend has to do is send a request to our new custom route, without worrying about sending any data in the body of the request.
+  patch "/birds/:id/like", to: "birds#increment_likes"
 end
